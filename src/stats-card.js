16bf91fa66ed253d92234ea574c927437087c0e0
Just-Prog <JustProg5835@outlook.com>
2026-04-22 10:42:47 +0800

    fix bugs from luogu's lentille change

--- src/stats-card.js
+++ src/stats-card.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 const {
-	Card,
-	renderError,
-	renderChart,
-	renderNameTitle,
+  Card,
+  renderError,
+  renderChart,
+  renderNameTitle,
 } = require("./common.js");
 
 /**
@@ -12,97 +12,112 @@ const {
  * @returns {Object} 获取的用户数据 {name, color, ccfLevel, passed, hideInfo}
  */
 async function fetchStats(id) {
-	let headers = { Cookie: `__client_id=${process.env.cid};_uid=${process.env.uid}` };
-	// console.log(headers);
-	const res = await axios.get(`https://www.luogu.com.cn/user/${id}?_contentOnly`, { headers });
+  let headers = {
+    Cookie: `__client_id=${process.env.cid};_uid=${process.env.uid}`,
+    "x-lentille-request": "content-only",
+    // "user-agent":
+    //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+  };
+  // console.log(headers);
+  const res = await axios.get(`https://www.luogu.com.cn/user/${id}`, {
+    headers,
+  });
 
-	const stats = {
-		name: "NULL",
-		color: "Gray",
-		ccfLevel: 0,
-		passed: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-		hideInfo: false,
-		ranking: 0,
-	};
-	if (res.data.code !== 200) {
-		return stats;
-	}
+  const stats = {
+    name: "NULL",
+    color: "Gray",
+    ccfLevel: 0,
+    passed: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    hideInfo: false,
+    ranking: 0,
+  };
+  if (res.status !== 200) {
+    return stats;
+  }
 
-	const user = res.data.currentData.user;
-	const passed = res.data.currentData.passedProblems;
+  const user = res.data.data.user;
+  const passed = res.data.data.passedProblemsCount;
 
-	stats.name = user.name;
-	stats.color = user.color;
-	stats.ccfLevel = user.ccfLevel;
+  stats.name = user.name;
+  stats.color = user.color;
+  stats.ccfLevel = user.ccfLevel;
+  stats.passed = Array.from({ length: 9 }, () => 0);
 
-	if (!passed) {
-		stats.hideInfo = true;
-		return stats;
-	}
+  if (passed === null) {
+    stats.hideInfo = true;
+    return stats;
+  }
 
-	for (let i of passed) {
-		stats.passed[i.difficulty]++;
-	}
+  const practiceRes = await axios.get(
+    `https://www.luogu.com.cn/user/${id}/practice`,
+    {
+      headers,
+    },
+  );
 
-	stats.passed[8] = res.data.currentData.submittedProblems.length;
+  for (let i of practiceRes.data.data.passed) {
+    stats.passed[i.difficulty]++;
+  }
 
-	stats.ranking = res.data.currentData.user.ranking;
-	
-	stats.uid = id;
+  stats.passed[8] = practiceRes.data.data.submitted.length;
 
-	return stats;
+  stats.ranking = res.data.data.user.ranking;
+
+  stats.uid = id;
+
+  return stats;
 }
 
 const renderSVG = (stats, options) => {
-	const { name, color, ccfLevel, passed, hideInfo, ranking, uid } = stats;
+  const { name, color, ccfLevel, passed, hideInfo, ranking, uid } = stats;
 
-	const { hideTitle, darkMode, cardWidth = 500 } = options || {};
+  const { hideTitle, darkMode, cardWidth = 500 } = options || {};
 
-	if (hideInfo) {
-		return renderError("用户开启了“完全隐私保护”，获取数据失败");
-	}
+  if (hideInfo) {
+    return renderError("用户开启了“完全隐私保护”，获取数据失败");
+  }
 
-	const paddingX = 25;
-	const labelWidth = 90; //柱状图头部文字长度
-	const progressWidth = cardWidth - 2 * paddingX - labelWidth - 60; //500 - 25*2(padding) - 90(头部文字长度) - 60(预留尾部文字长度)，暂时固定，后序提供自定义选项;
+  const paddingX = 25;
+  const labelWidth = 90; //柱状图头部文字长度
+  const progressWidth = cardWidth - 2 * paddingX - labelWidth - 60; //500 - 25*2(padding) - 90(头部文字长度) - 60(预留尾部文字长度)，暂时固定，后序提供自定义选项;
 
-	const weights = [60, 1, 2, 3, 10, 20, 40, 80, 0];
-	let score = 0;
+  const weights = [60, 1, 2, 3, 10, 20, 40, 80, 0];
+  let score = 0;
 
-	for (let i = 0; i < 8; ++i) score += weights[i] * passed[i];
+  for (let i = 0; i < 8; ++i) score += weights[i] * passed[i];
 
-	const datas = [
-		{ label: "未评定", color: "#bfbfbf", data: passed[0] },
-		{ label: "入门", color: "#fe4c61", data: passed[1] },
-		{ label: "普及-", color: "#f39c11", data: passed[2] },
-		{ label: "普及/提高-", color: "#ffc116", data: passed[3] },
-		{ label: "普及+/提高", color: "#52c41a", data: passed[4] },
-		{ label: "提高+/省选-", color: "#3498db", data: passed[5] },
-		{ label: "省选/NOI-", color: "#9d3dcf", data: passed[6] },
-		{ label: "NOI/NOI+/CTSC", color: "#0e1d69", data: passed[7] },
-		{ label: "写挂了", color: "#996600", data: passed[8] },
-	];
-	const passedSum = passed.reduce((a, b) => a + b) - passed[8];
-	const body = renderChart(datas, labelWidth, progressWidth, "题");
+  const datas = [
+    { label: "未评定", color: "#bfbfbf", data: passed[0] },
+    { label: "入门", color: "#fe4c61", data: passed[1] },
+    { label: "普及-", color: "#f39c11", data: passed[2] },
+    { label: "普及/提高-", color: "#ffc116", data: passed[3] },
+    { label: "普及+/提高", color: "#52c41a", data: passed[4] },
+    { label: "提高+/省选-", color: "#3498db", data: passed[5] },
+    { label: "省选/NOI-", color: "#9d3dcf", data: passed[6] },
+    { label: "NOI/NOI+/CTSC", color: "#0e1d69", data: passed[7] },
+    { label: "写挂了", color: "#996600", data: passed[8] },
+  ];
+  const passedSum = passed.reduce((a, b) => a + b) - passed[8];
+  const body = renderChart(datas, labelWidth, progressWidth, "题");
 
-	const title = renderNameTitle(
-		name,
-		color,
-		ccfLevel,
-		"的贺题情况",
-		cardWidth,
-		`已贺${passedSum}题, 被${ranking ? ranking - 1 : "INF"}人吊打`
-	);
+  const title = renderNameTitle(
+    name,
+    color,
+    ccfLevel,
+    "的贺题情况",
+    cardWidth,
+    `已贺${passedSum}题, 被${ranking ? ranking - 1 : "INF"}人吊打`,
+  );
 
-	return new Card({
-		width: cardWidth - 2 * paddingX,
-		height: datas.length * 30 + 10,
-		hideTitle,
-		darkMode,
-		title,
-		body,
-		score,
-	}).render();
+  return new Card({
+    width: cardWidth - 2 * paddingX,
+    height: datas.length * 30 + 10,
+    hideTitle,
+    darkMode,
+    title,
+    body,
+    score,
+  }).render();
 };
 
 module.exports = { fetchStats, renderSVG };
